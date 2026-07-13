578735753d57df9561a9e0c299f9ed771e084d23
padocity <tyem9721@gmail.com>
2026-07-13 18:36:33 +0900

    Scene updated

--- main.cc
+++ main.cc
@@ -4,13 +4,22 @@
 // Scene-specific objects
 #include "Hittable.h"
 #include "HittableList.h"
+#include "Material.h"
 #include "Sphere.h"
 
 int main() {
     // World
     HittableList world;
-    world.Add(make_shared<Sphere>(Point3(0, 0, -1), 0.5));
-    world.Add(make_shared<Sphere>(Point3(0, -100.5, -1), 100));
+
+    auto matGround = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
+    auto matCenter = make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
+    auto matLeft = make_shared<Metal>(Color(0.8, 0.8, 0.8));
+    auto matRight = make_shared<Metal>(Color(0.8, 0.6, 0.2));
+
+    world.Add(make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, matGround));
+    world.Add(make_shared<Sphere>(Point3(0.0, 0.0, -1.2), 0.5, matCenter));
+    world.Add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, matLeft));
+    world.Add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, matRight));
 
     // Camera
     Camera cam;
